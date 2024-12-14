d4959ee3951fd11856cf4e268c75a4592f150cbc
James <jatupat.ji@gmail.com>
2024-12-14 20:40:57 +0700

    Please

---
+++ Prior Website/Leaving/My Leave Calendar/Leave Request Main.cy.js
@@ -0,0 +1,8 @@
+import { createLeave,cancelLeave, pageAssertion } from './Function/Function.cy.js';
+
+// Step 1 : Page Assertion
+pageAssertion();
+// Step 2 : Create Leave
+// createLeave();
+// // Step 3: Cancel Leave
+// cancelLeave();
